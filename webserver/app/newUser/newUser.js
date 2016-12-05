92e8b3434fc58710039dec688b9063d75de7d73b
sven-of-the-north <zhangss@ucalgary.ca>
2016-12-04 19:19:54 -0700

    Added stylesheet and back button to create new user page

--- webserver/app/newUser/newUser.js
+++ webserver/app/newUser/newUser.js
@@ -5,33 +5,36 @@ newUserApp.config(['$cookiesProvider', function($cookiesProvider) {
 		$cookiesProvider.defaults.domain='localhost';
 	}]);
 	
-newUserApp.controller('newUserCtrl', ['$scope', '$cookies',
-	function($scope, $cookies) {
-		$scope.user = {
-			username: "username", 
-			password: "password",
-			type: ""};
-		$scope.retrieved = "";
-	
-		$scope.createNewUser = function() {
-			var newUser = {
-				username: $scope.user.username,
-				password: $scope.user.password,
-				type: $scope.user.type
-			};
-			
-			var retrievedList = $cookies.getObject('userList');
-			if( typeof retrievedList == 'undefined' )
-			{
-				var newList = [newUser];
-				$cookies.putObject('userList', newList);
-			}
-			else
-			{
-				retrievedList.push(newUser);
-				$cookies.putObject('userList', retrievedList);
-			}
-			
-			$scope.retrieved = $cookies.getObject('userList');
+newUserApp.controller('newUserCtrl', ['$scope', '$cookies', function($scope, $cookies) {
+	$scope.user = {
+		username: "username", 
+		password: "password",
+		type: ""};
+	$scope.retrieved = "";
+
+	$scope.createNewUser = function() {
+		var newUser = {
+			username: $scope.user.username,
+			password: $scope.user.password,
+			type: $scope.user.type
 		};
+		
+		var retrievedList = $cookies.getObject('userList');
+		if( typeof retrievedList == 'undefined' )
+		{
+			var newList = [newUser];
+			$cookies.putObject('userList', newList);
+		}
+		else
+		{
+			retrievedList.push(newUser);
+			$cookies.putObject('userList', retrievedList);
+		}
+		
+		$scope.retrieved = $cookies.getObject('userList');
+	};
+	
+	$scope.backToLogin = function() {
+		location.href = '/'
+	}
 }]);

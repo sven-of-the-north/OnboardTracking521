0faf3f04e0ca556e786c39110ed3b16653a1e935
Sven Zhang <zhangss@ucalgary.ca>
2016-12-04 23:42:13 -0700

    confirmed datepicker works, and test button available

--- webserver/app/dataPage/dataPage.js
+++ webserver/app/dataPage/dataPage.js
@@ -1,5 +1,10 @@
 angular
 	.module('driverPageApp', ['moment-picker'])
 	.controller('driverPageCtrl', ['$scope', function($scope) {
+		
+		
+		$scope.test = function () {
+			console.log($scope.ctrl);
+		}
 	}
 ]);

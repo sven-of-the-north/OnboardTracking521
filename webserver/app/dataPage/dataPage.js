8914d854af9f45e39589052c39fa257c99f3f2be
Sven Zhang <zhangss@ucalgary.ca>
2016-12-05 11:51:23 -0700

    Added dataPage get request to database server and initial population of table

--- webserver/app/dataPage/dataPage.js
+++ webserver/app/dataPage/dataPage.js
@@ -1,34 +1,37 @@
 angular
 	.module('driverPageApp', ['moment-picker'])
-	.config(['$sceDelegateProvider', function($sceDelegateProvider) {
-		$sceDelegateProvider.resourceUrlWhitelist([
-		'self',
-		'http://localhost:8080',
-		'http://localhost:8081' ])}
-	])
-	.controller('driverPageCtrl', ['$scope', '$http', '$templateCache', 
-	function($scope, $http, $templateCache) {
-		
-/** standard get request **/
-//		$http({
-//			method: 'GET',
-//			url: 'http://localhost:8080/events'
-//		}).then( function successCallback(response) {
-//			console.log(response);
-//		}, function errorCallback(response)
-//		{
-//			console.log(response);
-//		});
+	.controller('driverPageCtrl', ['$scope', '$http', function($scope, $http) {
+		$http({
+			method: 'GET',
+			url: 'http://localhost:8080/events'
+		}).then( function successCallback(response) {
+			$scope.eventList = response.data._embedded.event;
+			$scope.dataList = [];
+			
+			for( i = 0 ; i < $scope.eventList.length; i++ )
+			{
+				$scope.dataList.push( {  
+					carId: $scope.eventList[i].carId,
+					timeStamp: $scope.eventList[i].time,
+					eventName: $scope.eventList[i].eventName,
+					value: $scope.eventList[i].value
+				});
+			}
+		}, function errorCallback(response)
+		{
+			alert('Error retrieving event log');
+			console.log(response);
+		});
 
 /** using CORS **/		
-		var request = createCORSRequest("get", "http://localhost:8080/events/");
-		if (request){
-			request.onload = function() {
-				console.log(request);
-			};
-
-			request.send();
-		}
+//		var request = createCORSRequest("get", "http://localhost:8080/events/");
+//		if (request){
+//			request.onload = function() {
+//				console.log(request);
+//			};
+//
+//			request.send();
+//		}
 		
 		$scope.test = function () {
 			console.log($scope.ctrl);
@@ -37,15 +40,15 @@ angular
 ]);
 
 /** using CORS **/		
-function createCORSRequest(method, url){
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr){
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined"){
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
-}
+//function createCORSRequest(method, url){
+//    var xhr = new XMLHttpRequest();
+//    if ("withCredentials" in xhr){
+//        xhr.open(method, url, true);
+//    } else if (typeof XDomainRequest != "undefined"){
+//        xhr = new XDomainRequest();
+//        xhr.open(method, url);
+//    } else {
+//        xhr = null;
+//    }
+//    return xhr;
+//}

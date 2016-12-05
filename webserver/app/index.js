ece2d46b1f2f6b258ba9a3755f2550ed15df500d
Sven Zhang <zhangss@ucalgary.ca>
2016-12-04 18:11:15 -0700

    Login page now checks cookies for proper login credentials

--- webserver/app/index.js
+++ webserver/app/index.js
@@ -1,13 +1,32 @@
-angular.module('loginPage', [])
-	.controller('loginCtrl', function($scope) {
+angular.module('loginPage', ['ngCookies'])
+	.controller('loginCtrl', ['$scope', '$cookies', function($scope, $cookies) {
     $scope.user = {
         username:"username", 
         password:"password"};
+	$scope.loginSuccess = false;
+	
     $scope.login = function() {
-        alert("Attemping login with this information: \nUsername: " + $scope.user.username + 
-                                                      " Password: " + $scope.user.password );
+		$scope.loginSuccess = false;
+		
+		var retrievedList = $cookies.getObject('userList');
+		if ( typeof retrievedList == 'undefined' )
+			return;
+		
+		for( i = 0; i < retrievedList.length; ++i )
+		{
+			if( retrievedList[i].username == $scope.user.username &&
+				retrievedList[i].password == $scope.user.password )
+				{
+					$scope.loginSuccess = true;	// TODO: implement actual login here.
+					break;
+				}
+		}
+		
+		if ( $scope.loginSuccess == false )
+			alert("Invalid login information, please try again.");
+		
 	};
 	$scope.newUser = function() {
 		location.href = 'newUser/newUser.html';
 	};
-});
+}]);
